3909ca2ec0ee3f40a0a5a12c09d6293751325695
Alayande Esther <alayandeesther469@gmail.com>
2025-10-04 02:17:17 +0100

    quick fix

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import Link from "next/link";
 import Image from "next/image";
